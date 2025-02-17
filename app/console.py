80621393fc961f0db59e0472346032d4c4756d5f
edwin sanabria <esanabriasalazasr1988@gmail.com>
2025-02-17 12:53:08 -0500

    PRIMERA CLASE

--- app/console.py
+++ app/console.py
@@ -1,1 +1,7 @@
 # TODO: Agrega el código de las clases de la interfaz de usuario aquí. Borra este comentario al terminar.
+class note:
+    def __init__(self, HIGH: str, MEDIUM: str, LOW:str):
+            self.HIGH: str = HIGH
+            self.MEDIUM: str = MEDIUM
+            self.LOW: str = LOW
+
