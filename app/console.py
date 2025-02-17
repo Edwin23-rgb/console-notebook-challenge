b815b9245c61785b09a03e59393ec4457cfcc120
edwin sanabria <esanabriasalazasr1988@gmail.com>
2025-02-17 13:31:15 -0500

    atributos de la clase

--- app/console.py
+++ app/console.py
@@ -1,7 +1,19 @@
 # TODO: Agrega el código de las clases de la interfaz de usuario aquí. Borra este comentario al terminar.
-class note:
-    def __init__(self, HIGH: str, MEDIUM: str, LOW:str):
-            self.HIGH: str = HIGH
-            self.MEDIUM: str = MEDIUM
-            self.LOW: str = LOW
+from datetime import datetime
+
+
+class Note:
+
+    HIGH = 'HIGH'
+    MEDIUM = 'MEDIUM'
+    LOW = 'LOW'
+
+    def __init__(self,code: str,title: str,text: str,importance: str):
+        self.code: str= code
+        self.title: str=title
+        self.text: str=text
+        self.importance: str=importance
+        self.creation_date = datetime.now()
+
+
 
